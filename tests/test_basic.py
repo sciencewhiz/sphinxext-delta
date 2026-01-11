52bd012620fe7011dcc6bcc087122623d51c7576
sciencewhiz <sciencewhiz@users.noreply.github.com>
2026-01-10 21:03:52 -0800

    Actually build in test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -5,4 +5,16 @@ from sphinx import version_info
 
 @pytest.mark.sphinx("html", testroot="basic")
 def test_simple_short(app: Sphinx):
-    assert True
+    app.build()
+
+    content = read_text(app)
+
+    html = '<h1>test<a class="headerlink" href="#test" title="Link to this heading">Â¶</a></h1>'
+
+    assert html in content
+
+def read_text(app: Sphinx):
+    if version_info[:2] < (3, 0):
+        return (app.outdir / "index.html").text().replace("\n", "")
+    else:
+        return (app.outdir / "index.html").read_text()
